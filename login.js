64035c013d636979b5d3993e8ee9a5385163a8a6
Samuel Salazar <samuel.salazar1333@gmail.com>
2020-09-26 15:33:08 -0600

    second commit

--- login.js
+++ login.js
@@ -21,3 +21,21 @@ constructor(horsepower, turbo) {
 var SportsCar = new SportsCar(500)
 console.log(SportsCar)
 
+var Prices = [10, 25, 40];
+console.log("initial prices:");
+console.log(prices);
+
+var halve = function(val) {
+	return 0.5*val;
+}
+
+var reduce = function(arr) {
+	for(var i = 0; i< arr.length; i++)
+	{
+		console.log(halve(arr[i]))
+	}
+}
+
+console.log("reduced prices:");
+reduce(prices);
+
