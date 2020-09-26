1c6f32981d91a72c3525e41b24cadf34f3759607
Samuel Salazar <samuel.salazar1333@gmail.com>
2020-09-26 15:04:51 -0600

    updated

--- login.js
+++ login.js
@@ -3,3 +3,21 @@ function myfunction(){
 }
 
 alert("wow");
+
+class Car {
+	constructor(horsepower){
+		this.horsepower = horsepower;
+		this.doors = 4;
+	}
+}
+
+class SportsCar extends Car {
+constructor(horsepower, turbo) {
+	super(horsepower);
+	this.turbo = "yes";
+ }
+}
+
+var SportsCar = new SportsCar(500)
+console.log(SportsCar)
+
