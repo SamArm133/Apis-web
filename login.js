a955e0fd513ddc1978be8378e38fe90eca4f87fd
Samuel Salazar <samuel.salazar1333@gmail.com>
2020-11-02 22:19:02 -0600

    added scroll

--- login.js
+++ login.js
@@ -1,3 +1,14 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+	document.querySelector('.nav-main').classList.toggle('show');
+});
+
+
+ScrollReveal().reveal('.news');
+ScrollReveal().reveal('.box', {delay: 500});
+ScrollReveal().reveal('.banner1', {delay: 500});
+ScrollReveal().reveal('.box2', {delay: 500}); 
+ScrollReveal().reveal('.banner2', {delay: 500});
+
 var myfunction = function() {
 	document.getElementBy("demo").innerHTML = "IVA";
 }
